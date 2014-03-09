c5359877b1b79a70d881a0bcdbd34b0d4cdcb6a7
David Jacques <David_Jacques@cable.comcast.com>
2014-03-08 23:21:32 -0500

    updates

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-FILES="ackrc aliases bashrc bash_profile gemrc gitconfig tmux.conf vimrc"
+FILES="ackrc aliases bashrc bash_profile gemrc gitconfig tmux tmux.conf vimrc.after vimrc.before"
 
 DIR=~/dotfiles
 OLDDIR=~/dotfiles_old
--- vimrc
+++
@@ -1,2 +0,0 @@
-:set directory=$HOME/.vim/swapfiles//
-
